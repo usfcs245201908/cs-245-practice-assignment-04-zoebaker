e3386c484c3e205bb354b93d1578b1946fd23445
zoebaker <zoebaker31@gmail.com>
2019-09-19 21:59:36 -0700

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,18 +1,14 @@
 public class BubbleSort {
-	void sort (int [] a) {
-		boolean swapped = true; 
-		int j = 0; 
-		while (swapped) {
-			swapped = false; 
-			for (int i =0; i < a.length-1-j; i++) {
-				if (a[i] > a[i+1]) {
-					swap (a, i, i+1); 
-					swapped = true; 
-				}
-				j++; 
-			}
+	public void sort (int [] a) {
+		for (int i =0; i < a.length-1; i++) {
+			for (int j = 0; j < a.length-i-1; j++)
+				if (a[j] > a[j+1]) {
+				swap (a, j, j+1); 
+					 
+			}	
 		}
 	}
+	
 	
 	void swap (int [] a, int i, int j) {
 		int temp = a[i]; 
